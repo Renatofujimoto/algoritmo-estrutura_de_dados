7ee7f774da9df46ef9269428d41c53c57d808af3
Renato Fujimoto <renatofujimoto2@gmail.com>
2024-05-28 11:56:26 -0300

    chore: Refactor LinkedList class

--- my_solution_exercises/linkedlist/index.js
+++ my_solution_exercises/linkedlist/index.js
@@ -11,11 +11,11 @@ class Node {
 
 class LinkedList {
     constructor() {
-        this.head = null
+        this.head = null;
     }
 
     insertFirst(data) {
-        this.insertAt(data, 0)
+        this.head = new Node(data, this.head);
     }
 
     size() {
@@ -26,117 +26,103 @@ class LinkedList {
             counter++;
             node = node.next;
         }
+
         return counter;
     }
 
     getFirst() {
-        return this.getAt(0);
+        return this.head;
     }
 
     getLast() {
-        // if (!this.head) {
-        //     return null;
-        // }
+        if (!this.head) {
+            return null;
+        }
 
-        // let node = this.head;
-
-        // while (node) {
-        //     if (!node.next) {
-        //         return node;
-        //     }
-        //     node = node.next;
-        // }
-
-        return this.getAt(this.size() -1);
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+            node = node.next;
+        }
     }
 
     clear() {
-        this.head = null
+        this.head = null;
     }
 
     removeFirst() {
         if (!this.head) {
-            return
+            return;
         }
 
-        this.head = this.head.next
+        this.head = this.head.next;
     }
 
     removeLast() {
-
         if (!this.head) {
             return;
         }
 
         if (!this.head.next) {
-            return
+            this.head = null;
+            return;
         }
 
-        let previous = this.head
-        let node = this.head.next
-
-        while (!node.next) {
+        let previous = this.head;
+        let node = this.head.next;
+        while (node.next) {
             previous = node;
-            node = node.next
+            node = node.next;
         }
-
-        previous.next = null
+        previous.next = null;
     }
 
     insertLast(data) {
         const last = this.getLast();
 
         if (last) {
-            //there are some existing node in our chain
-
+            // There are some existing nodes in our chain
             last.next = new Node(data);
         } else {
-            //the chain is empty!
-            new Node(data);
+            // The chain is empty!
+            this.head = new Node(data);
         }
     }
 
     getAt(index) {
-
-        if (!this.head) {
-            return null
-        }
-
         let counter = 0;
         let node = this.head;
-
         while (node) {
             if (counter === index) {
                 return node;
             }
+
             counter++;
             node = node.next;
         }
-
         return null;
     }
 
     removeAt(index) {
-
         if (!this.head) {
             return;
         }
 
         if (index === 0) {
             this.head = this.head.next;
-            return
-        }
-
-        const previous = this.getAt(index - 1)
-        if (!previous || !previous.next) {
             return;
         }
 
+        const previous = this.getAt(index - 1);
+        if (!previous || !previous.next) {
+            return;
+        }
         previous.next = previous.next.next;
     }
 
     insertAt(data, index) {
-
         if (!this.head) {
             this.head = new Node(data);
             return;
@@ -150,29 +136,25 @@ class LinkedList {
         const previous = this.getAt(index - 1) || this.getLast();
         const node = new Node(data, previous.next);
         previous.next = node;
-
     }
 
     forEach(fn) {
         let node = this.head;
         let counter = 0;
-
         while (node) {
-            fn(node,counter);
+            fn(node, counter);
             node = node.next;
-            counter ++
+            counter++;
         }
     }
 
-    *[Symbol.interador] () {
+    *[Symbol.iterator]() {
         let node = this.head;
-
         while (node) {
             yield node;
             node = node.next;
         }
     }
-
 }
 
 // const list = new LinkedList();
